ef60dcbef92f6037de947e806572b371757d3d9c
Kilian Ciuffolo <kilian.ciuffolo@gmail.com>
2015-07-01 18:07:20 -0700

    feat: optional robots.txt

--- server.js
+++ server.js
@@ -7,7 +7,17 @@
 'use strict'
 
 const express = require('express')
+const NODE_ENV = process.env.NODE_ENV
 
 var app = express()
 app.use(express.static(__dirname + '/dist'))
+
+if ('prod' !== NODE_ENV) {
+  app.use(function (req, res, next) {
+    if ('/robots.txt' !== req.url) return next()
+    res.type('text/plain')
+    res.send('User-agent: *\nDisallow: /')
+  })
+}
+
 app.listen(8080)
